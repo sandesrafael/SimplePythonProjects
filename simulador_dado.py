1e927848e56b817fc3848645cc7376777f12d104
Rafael Sandes <raafaelsaandes@gmail.com>
2021-06-08 01:57:00 -0300

    Commit do segundo projeto em Python(INterface gráfica)

--- simulador_dado.py
+++ simulador_dado.py
@@ -1,30 +1,41 @@
 #Simulador de dado
 #Simular o uso de um dado, gerando um valor de 1 a 6
 import random
+import PySimpleGUI as sg
 
 class SimuladorDados:
     
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de gerar um novo valor para o dado?'
-    
-    def Iniciar(self):
+        #Criar layout
+        self.layout = [
+            [sg.Text('Jogar o dado?')],
+            [sg.Button('SIM'),sg.Button('NAO')],
+            [sg.Button('Encerrar')]
+        ]
         
-        resposta = input(self.mensagem)
+    def Iniciar(self):
+        # Criar uma janela
+        self.janela = sg.Window('Simulador de dado',layout=self.layout)
+        # Ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        # Fazer alguma coisa com esses valores
         try:
-            if resposta == 's' or resposta == 'sim':
-                self.GeraValorDado()
-            elif resposta == 'n' or resposta == 'nao':
-                print('Encerrando o programa...')
+            if self.eventos == 'SIM' or self.eventos == 's':
+                self.GerarValorDado()
+            elif self.eventos == 'NAO' or self.eventos == 'n':
+                print('Agradecemos sua participação.\n')
+            elif self.eventos == 'Encerrar':
+                    print('Encerrando o Jogo')
             else:
-                print('Favor digitar sim ou não')
+                print('Favor digitar SIM ou NAO')
         except:
             print('Ocorreu um erro ao receber sua resposta')
                 
-    def GeraValorDado(self):
+    def GerarValorDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))
-    
-    
+  
+
 simulador = SimuladorDados()
 simulador.Iniciar()
