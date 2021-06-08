8945f7044fe75f09bd5bc07ed0c21e0ed5bd2e35
Rafael Sandes <raafaelsaandes@gmail.com>
2021-06-08 00:04:01 -0300

    commit

--- simulador_dado.py
+++ simulador_dado.py
@@ -2,28 +2,29 @@
 #Simular o uso de um dado, gerando um valor de 1 a 6
 import random
 
-class SimuladorDado:
+class SimuladorDados:
+    
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = 'Você gostaria de gerar um novo valor para o dado?'
     
     def Iniciar(self):
+        
         resposta = input(self.mensagem)
         try:
-            if resposta == 'sim' or resposta == 's':
-                self.GerarValorDado()
-            elif resposta == 'nao' or resposta == 'n':
-                print('Agradecemos sua participação')
+            if resposta == 's' or resposta == 'sim':
+                self.GeraValorDado()
+            elif resposta == 'n' or resposta == 'nao':
+                print('Encerrando o programa...')
             else:
-                print('Favor digitar sim ou nao')
+                print('Favor digitar sim ou não')
         except:
             print('Ocorreu um erro ao receber sua resposta')
-            
-            
-    def GerarValorDado(self):
+                
+    def GeraValorDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))
-        
-
-simulador = SimuladorDado()
+    
+    
+simulador = SimuladorDados()
 simulador.Iniciar()
