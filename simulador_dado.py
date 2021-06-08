56fa5afd56ce832ff1dbbc23995fde3215cf8c09
Rafael Sandes <raafaelsaandes@gmail.com>
2021-06-08 02:03:30 -0300

    Commit

--- simulador_dado.py
+++ simulador_dado.py
@@ -1,23 +1,23 @@
-#Simulador de dado
-#Simular o uso de um dado, gerando um valor de 1 a 6
+# Simulador de dado
+# Simular o uso de um dado, gerando um valor de 1 a 6
 import random
 import PySimpleGUI as sg
 
+
 class SimuladorDados:
-    
     def __init__(self):
         self.valor_minimo = 1
         self.valor_maximo = 6
-        #Criar layout
+        # Criar layout
         self.layout = [
             [sg.Text('Jogar o dado?')],
-            [sg.Button('SIM'),sg.Button('NAO')],
+            [sg.Button('SIM'), sg.Button('NAO')],
             [sg.Button('Encerrar')]
         ]
-        
+
     def Iniciar(self):
         # Criar uma janela
-        self.janela = sg.Window('Simulador de dado',layout=self.layout)
+        self.janela = sg.Window('Simulador de dado', layout=self.layout)
         # Ler os valores da tela
         self.eventos, self.valores = self.janela.Read()
         # Fazer alguma coisa com esses valores
@@ -27,15 +27,15 @@ class SimuladorDados:
             elif self.eventos == 'NAO' or self.eventos == 'n':
                 print('Agradecemos sua participação.\n')
             elif self.eventos == 'Encerrar':
-                    print('Encerrando o Jogo')
+                print('Encerrando o Jogo')
             else:
                 print('Favor digitar SIM ou NAO')
         except:
             print('Ocorreu um erro ao receber sua resposta')
-                
+
     def GerarValorDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo))
-  
+
 
 simulador = SimuladorDados()
 simulador.Iniciar()
